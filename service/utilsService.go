b11260fed1c2c099c784846f14cc6e73b7043aa9
Adrian Clepcea <adi_clepcea@yahoo.com>
2017-12-03 21:21:53 +0200

    Add operations for 4 destinations

--- service/utilsService.go
+++ service/utilsService.go
@@ -2,12 +2,25 @@ package service
 
 import (
 	"net/http"
-	"github.com/adiclepcea/furnir/pdf"
 	"net/http/httputil"
+
+	"github.com/adiclepcea/furnir/pdf"
 )
+
 //PrintOperations generates a PDF with the barcodes used for operations
 func PrintOperations(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-type","application/pdf")
-	operationBarcodes := []string{"palet sursa", "palet dest","print sursa", "print dest","palet nou"}
-	pdf.GenerateSimplePDF(operationBarcodes, "Operatii paleti", httputil.NewChunkedWriter(w))	
+	w.Header().Add("Content-type", "application/pdf")
+	operationBarcodes := []string{
+		"palet sursa",
+		"palet dest1",
+		"palet dest2",
+		"palet dest3",
+		"palet dest4",
+		"print sursa",
+		"print dest1",
+		"print dest2",
+		"print dest3",
+		"print dest4",
+		"palet nou"}
+	pdf.GenerateSimplePDF(operationBarcodes, "Operatii paleti", httputil.NewChunkedWriter(w))
 }
